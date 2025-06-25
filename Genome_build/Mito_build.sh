44f9d6c54bd91fd3e10c3365c90dbb27deaf2d28
Emily Curd <eecurd@g.ucla.edu>
2025-06-25 13:17:02 -0400

    Update and rename Mito_build.md to Mito_build.sh

--- Genome_build/Mito_build.sh
+++ Genome_build/Mito_build.sh
@@ -1,10 +1,11 @@
-path=~/concatonated_short_reads/
+# set varaibles for path to short reads used to make the genome and the sample name
+path=~/concatonated_short_reads/ 
 sample_name=HL4
 
-## Assemble mitogenome
+# Assemble mitogenome with getOrganelle
 get_organelle_from_reads.py -1 ${path}/${sample_name}_all_R1_001.fastq.gz \
 			    -2 ${path}/${sample_name}_all_R2_001.fastq.gz \
            -R 10 -k 21,45,65,85,105 -F animal_mt -o ~/getorganelle/${sample_name}_mt_out --target-genome-size 17000  
 
-## Standardize origin and annotate
+#Standardize origin and annotate mtgrasp
 mtgrasp_standardize.py -i ~/getorganelle/HL4_mt_out/HL4_complete_mitogenome.fasta -o ${sample_name}_stand  -c 2 -p ${sample_name}_ann -a -mp ~/miniforge3/bin
