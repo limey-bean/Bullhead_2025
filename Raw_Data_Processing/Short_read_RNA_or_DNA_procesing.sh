8c0af4760df8ede26e0ba83d9033029f7302f523
Emily Curd <eecurd@g.ucla.edu>
2025-06-26 09:31:13 -0400

    Update Short_read_RNA_or_DNA_procesing.sh

--- Raw_Data_Processing/Short_read_RNA_or_DNA_procesing.sh
+++ Raw_Data_Processing/Short_read_RNA_or_DNA_procesing.sh
@@ -1,16 +1,24 @@
+#######
 # Set variables
+#######
 
 sample_name=<sample_name>
 
-in=~/raw_concatonated_short_reads/
-fastqc_out=~/short_reads/fastqc_short/
-fastp_out=~/short_reads/fastp/
+in=<path to raw concatonated short reads>
+fastqc_out=<path to fastqc output directory>
+fastp_out=<path to fastp output directory>
 
+#######
 # make directories
+#######
+
 mkdir -p ${in}
 mkdir -p ${fastqc_out}
 mkdir -p ${fastp_out}
 
+######
+# Concatonate raw fastq data (if necessary)
+######
 
 # If there are multiple sequence output per sample, concatonate by forward and reverse reads:
 
@@ -22,10 +30,13 @@ find ~/raw_data_dump_dir/  -type f -name "${sample_name}_*R2_001*" > ${workdir}/
 { xargs cat < ${workdir}/${sample_name}_R1_filepaths.txt ; } > ${workdir2}/${sample_name}_all_R1_001.fastq.gz
 { xargs cat < ${workdir}/${sample_name}_R2_filepaths.txt ; } > ${workdir2}/${sample_name}_all_R2_001.fastq.gz
 
-
+########
 # Run fastqc on all samples
+########
 fastqc ${in}* -o ${fastqc_out}
 
+########
 # Run fastp on all samples
+########
 fastp -i ${in}/${sample_name}_all_R1_001.fastq.gz -I ${in}/${sample_name}_all_R2_001.fastq.gz -o ${fastp_out}${sample_name}_R1_001.fastq.gz -O ${fast_pout}${sample_name}_R2_001.fastq.gz -h ${fastp_out}${sample_name}_fastp.html -j ${fastp_out}${sample_name}_fastp.json
 
