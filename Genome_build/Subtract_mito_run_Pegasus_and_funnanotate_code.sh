961ec2a151cece1a332fc54cefc1bb2d3ba64900
Emily Curd <eecurd@g.ucla.edu>
2025-06-26 10:24:46 -0400

    Update Subtract_mito_run_Pegasus_and_funnanotate_code.sh

--- Genome_build/Subtract_mito_run_Pegasus_and_funnanotate_code.sh
+++ Genome_build/Subtract_mito_run_Pegasus_and_funnanotate_code.sh
@@ -68,16 +68,100 @@ bedtools bamtofastq -i ${dir_path}/${sample}/${sample}_no_mito_only.bam -fq ${di
 # command to run Pegasus. For more details see: https://github.com/jaxlub/PEGASUS
 
 ~/PEGASUS/pegasus.sh \
-            -n ~/HL4/Long_reads/HL4_no_mito.fastq.gz \
+            -n ${dir_path}/${sample}/Long_reads/HL4_no_mito.fastq.gz \
             -s1 <path to concatonated short_reads1>  \
             -s2 <path to concatonated short_reads2> \
             -phv ~/PHVindexes\ # centrifuge database for removing contamination
             -b ~/actinopterygii_odb10\ # Busco database for fish
             -qg ~/Ameiurus_melas_genome/GCA_012411365.1_AMELA_1.0_genomic.gff \ # genome of closely related species - for additional scaffolding - we did not use results because A melas has a different number of chromosomes thatn A. nebulosa
             -qf ~/Ameiurus_melas_genome/GCA_012411365.1_AMELA_1.0_genomic.fna \
-            -r ~/HL4RefGen.fasta
+            -r ${dir_path}/${sample}/HL4_genome_no_mitochondria.fasta
 
 
 ######################################
 # Genome Annotation using Funnanotate
+# 
+
+#add genome to the following directory with the genome name specified below ${genome}.fasta
+
+cd ${dir_path}/${sample}/
+
+genomedir=${dir_path}/${sample}/
+genome=HL4_genome_no_mitochondria
+RNAseq_path1R1=<path to clean RNAseq data sample 1 R1>
+RNAseq_path1R2=<path to clean RNAseq data sample 1 R2>
+RNAseq_path2R1=<path to clean RNAseq data sample 2 R1>
+RNAseq_path2R2=<path to clean RNAseq data sample 2 R2>
+....
+
+# I used RNAseq samples vt1_03, vt2_10, vt1_07, vt2_23, vt1_02, vt2_06, vt2_32, vt2_29R, vt2_05
+
+#######################
+# clean up names in genome file
+#######################
+
+# remove artifacts in contig file
+sed 's/JAAGNN01000//g' ${genomedir}${genome}.fasta > ${genomedir}${genome}.1.fasta
+sed 's/_polished//g' ${genomedir}${genome}.1.fasta > ${genomedir}${genome}.2.fasta
+
+# use funannotate's cleanup function  remove all contigs < 1000 bp
+funannotate \
+   clean -i ${genomedir}${genome}.2.fasta --minlen 1000 -o ${genomedir}${genome}.cleaned.fa
+
+# sort contigs by length
+seqkit sort -l --reverse --two-pass ${genomedir}${genome}.cleaned.fa > ${genomedir}${genome}.cleaned.sorted.fa
+
+# rename reads
+seqkit replace -p .+ -r HL4_{nr} --nr-width 4 ${genomedir}${genome}.cleaned.sorted.fa -o ${genomedir}${genome}.cleaned.sorted.renamed.fa
+
+# remove superfluous files
+rm ${genomedir}${genome}.1.fasta
+rm ${genomedir}${genome}.2.fasta
+rm ${genomedir}${genome}.cleaned.fa
+rm ${genomedir}${genome}.cleaned.sorted.fa
+
+
+#######################
+# mask assembly....
+#######################
+
+funannotate \
+  mask -i ${genomedir}${genome}.cleaned.sorted.renamed.fa  --cpus 20 -o ${genomedir}${genome}_masked_assembly.fa
+
+######################
+# train gene model with rnaseq data
+######################
+
+
+ funannotate \
+    train -i ${genomedir}${genome}_masked_assembly.fa -o ${genome}Fun_out \
+    --left ${RNAseq_path1R1} ${RNAseq_path2R1} ... \
+    --right ${RNAseq_path1R2} ${RNAseq_path2R2} ... \
+    --stranded RF --jaccard_clip --cpus 20
+
+######################
+# predict genes
+######################
+
+ funannotate \
+    predict -i ${genomedir}${genome}_masked_assembly.fa  -o ${genome}Fun_out -s "HL4_no_scaffold_masked_assembly" \
+    --cpus 20 --busco_seed_species zebrafish  --busco_db ${path_toFunannotate_db}/actinopterygii --organism  other --optimize_augustus --database ${path_toFunannotate_db}
+
+######################
+# update model
+######################
+
+funannotate \
+    update -i ${genome}Fun_out --cpus 20
+
+######################
+# annotate final 
+######################
+# I ran the ${genome}Fun_out/predict_results/HL4_no_scaffold_masked_assembly.proteins.fa through the eggnog_mapper annotation webserver (http://eggnog-mapper.embl.de) and added the output to:
+mkdir ${genome}Fun_out/eggnog_mapper_out/
+
+funannotate \
+     annotate -i ${genome}Fun_out --cpus 20 --eggnog ${genome}Fun_out/eggnog_mapper_out/out.emapper.annotations \
+     --isolate HL4 --busco_db ${path_toFunannotate_db}/actinopterygii -d ${path_toFunannotate_db} --species "Ameiurus nebulosus"
+
 
