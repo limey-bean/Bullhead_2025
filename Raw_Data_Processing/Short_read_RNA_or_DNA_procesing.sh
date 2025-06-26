0bb843c9d67d20e6b3ddb76c0b898935cb00c67d
Emily Curd <eecurd@g.ucla.edu>
2025-06-26 08:50:02 -0400

    Update Short_read_RNA_or_DNA_procesing.sh

--- Raw_Data_Processing/Short_read_RNA_or_DNA_procesing.sh
+++ Raw_Data_Processing/Short_read_RNA_or_DNA_procesing.sh
@@ -1,17 +1,31 @@
+# Set variables
 
-# All samples with multiple sequence output were concatonated by forward or reverse reads using cat and stored in ~/raw_concatonated_short_reads/
+sample_name=<sample_name>
 
 in=~/raw_concatonated_short_reads/
 fastqc_out=~/short_reads/fastqc_short/
 fastp_out=~/short_reads/fastp/
 
-sample_name=<sample name>
+# make directories
+mkdir -p ${in}
+mkdir -p ${fastqc_out}
+mkdir -p ${fastp_out}
+
+
+# If there are multiple sequence output per sample, concatonate by forward and reverse reads:
+
+## compile a list of all raw reads for a sample, below I am calling the place where I store my raw reads "~/raw_data_dump_dir/":
+find ~/raw_data_dump_dir/ -type f -name "${sample_name}_*R1_001*" > ${workdir}/${sample_name}_r_R1_filepaths.txt
+find ~/raw_data_dump_dir/  -type f -name "${sample_name}_*R2_001*" > ${workdir}/${sample_name}_r_R2_filepaths.txt
+
+## concatonate reads
+{ xargs cat < ${workdir}/${sample_name}_R1_filepaths.txt ; } > ${workdir2}/${sample_name}_all_R1_001.fastq.gz
+{ xargs cat < ${workdir}/${sample_name}_R2_filepaths.txt ; } > ${workdir2}/${sample_name}_all_R2_001.fastq.gz
+
 
 # Run fastqc on all samples
 fastqc ${in}* -o ${fastqc_out}
 
-in=~/raw_concatonated_short_reads/
-out=~/short_reads/fastp/
-
+# Run fastp on all samples
 fastp -i ${in}/${sample_name}_all_R1_001.fastq.gz -I ${in}/${sample_name}_all_R2_001.fastq.gz -o ${fastp_out}${sample_name}_R1_001.fastq.gz -O ${fast_pout}${sample_name}_R2_001.fastq.gz -h ${fastp_out}${sample_name}_fastp.html -j ${fastp_out}${sample_name}_fastp.json
 
