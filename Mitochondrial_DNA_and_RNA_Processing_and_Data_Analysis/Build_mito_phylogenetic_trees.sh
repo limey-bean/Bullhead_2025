7ad718281dd3d807bbd5c9a0068a13ebf5d0a5f8
Emily Curd <eecurd@g.ucla.edu>
2025-07-01 20:08:22 -0400

    Update Build_mito_phylogenetic_trees.sh

--- Mitochondrial_DNA_and_RNA_Processing_and_Data_Analysis/Build_mito_phylogenetic_trees.sh
+++ Mitochondrial_DNA_and_RNA_Processing_and_Data_Analysis/Build_mito_phylogenetic_trees.sh
@@ -61,9 +61,54 @@ library(aplot)
 library("wesanderson")
 library(scales)
 
-
+# load datafiles for the tree, meta data, and snps
 tree <- read.tree(file="path_to/mito_reads.align.fasta.contree")
+tree_labels <- read.csv("path_to/meta.csv",  header = TRUE)
+snps <- read.delim("path_to/snps.txt", header = TRUE)
 
+##format of the metadatafile:
+#              name sample          Location            Tumor Pair          site MultiMito tumor_type percent_mito  libtype TumorP oldcall   call 
+# 1  BS1_Hap1_2N_vcf    BS1     Lake_Bomoseen Normal_Haplotype <NA> Lake_Bomoseen         N       <NA>          100       SL   <NA>     BS1 BS1-SL
+# 2  BS4_Hap1_2N_vcf    BS4     Lake_Bomoseen Normal_Haplotype <NA> Lake_Bomoseen         N       <NA>          100       SL   <NA>     BS4 BS4_SL
+# 3  FB1_Hap1_2N_vcf  FB1.2 Lake_Memphremagog Normal_Haplotype <NA>     Fitch_bay         Y       <NA>           75 ShortDNA   <NA>   FB1.2  FB1.2
+# 4  FB1_Hap2_2N_vcf  FB1.1 Lake_Memphremagog Normal_Haplotype <NA>     Fitch_bay         Y       <NA>           25 ShortDNA   <NA>   FB1.1  FB1.1
+# 5 FB11_Hap1_2N_vcf   FB11 Lake_Memphremagog Normal_Haplotype <NA>     Fitch_bay         N       <NA>          100 ShortDNA   <NA>    FB11   FB11
+# 6 FB12_Hap1_2N_vcf   FB12 Lake_Memphremagog Normal_Haplotype <NA>     Fitch_bay         N       <NA>          100 ShortDNA   <NA>    FB12   FB12
+# ....
+
+# join the tree data with metadata
+x <- left_join(as_tibble(tree), tree_labels, by = c("label" = "name"))  %>% mutate(name=label)
+
+# format SNP data
+snps <- snps %>% remove_rownames() %>% column_to_rownames(var="X")
+gapChar <- "?"
+snp <- t(snps)
+lsnp <- apply(snp, 1, function(x) {
+  x != snp[1,] & x != gapChar & snp[1,] != gapChar
+})
+lsnp <- as.data.frame(lsnp)
+lsnp$pos <- as.numeric(rownames(lsnp))
+lsnp <- tidyr::gather(lsnp, name, value, -pos)
+snp_data <- lsnp[lsnp$value, c("name", "pos")]
+snp_data <- snp_data 
+
+# Build tree
+p <- ggtree(tree) %<+% x  + 
+  geom_tippoint(aes(color = TumorP, shape=libtype),size=2)  + coord_cartesian(clip = 'off') + geom_tiplab(aes(label=call, color=TumorP, fontface="bold"),size=2.4, align=TRUE, linesize=.1) +
+  geom_nodelab( aes(label=label, subset=label > 79), hjust = 1.44, nudge_y = 0.65, size = 3) +
+  geom_nodelab( aes(label=label, subset=label ==100 ), hjust = 1.44, nudge_y = 0.65, size = 3) + coord_cartesian(clip = 'off') + 
+  theme_tree2(plot.margin=margin(10, 200, 10, 10))  + 
+  theme(axis.text.x = element_text(face="bold", size = 10)) +
+  scale_color_manual(values=c("#3A9AB2","#3A9AB2","orange1","#F11B00","#3A9AB2","#3A9AB2","orange1")) + theme(legend.position="none") 
+p
+
+# add snp panel to tree
+p1 <- p + geom_facet(panel = "SNP", data = snp_data, geom = geom_point, 
+               mapping=aes(x = pos, color = TumorP), shape = 16) + 
+  theme_tree2(plot.margin=margin(5, 200, 5, 5),)   + 
+  theme(axis.text.x = element_text(face="bold", size = 10))
+
+p1
 
 
 ######################################
