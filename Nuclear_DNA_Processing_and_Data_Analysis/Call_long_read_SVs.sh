edaea3a63566e239418575c7de42a80d7a3ef5c9
Emily Curd <eecurd@g.ucla.edu>
2025-07-02 18:52:08 -0400

    Update Call_long_read_SVs.sh

--- Nuclear_DNA_Processing_and_Data_Analysis/Call_long_read_SVs.sh
+++ Nuclear_DNA_Processing_and_Data_Analysis/Call_long_read_SVs.sh
@@ -1,1 +1,15 @@
 
+
+
+sample=<sample name>
+
+out=/path_to_output_for/sniffles/
+
+datadir=/path_to/map_long/
+ref_genome=/path_to/HL4.scaffolds.fa
+
+mkdir ${out}
+
+sniffles --input ${datadir}/${sample}/${sample}_aligned_sorted_filtered.bam --snf ${out}/${sample}.snf
+
+sniffles --input ${out}/${sample1}.snf ${out}/${sample2}.snf ... ${out}/${sampleN}.snf --vcf sniffles_all.vcf
