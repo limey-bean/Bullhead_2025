c927bbb1ca6a8146a688f5470abcb8ca89494b3e
Emily Curd <eecurd@g.ucla.edu>
2025-07-02 19:34:11 -0400

    Update Build_mito_phylogenetic_trees.sh

--- Mitochondrial_DNA_and_RNA_Processing_and_Data_Analysis/Build_mito_phylogenetic_trees.sh
+++ Mitochondrial_DNA_and_RNA_Processing_and_Data_Analysis/Build_mito_phylogenetic_trees.sh
@@ -118,15 +118,21 @@ p1
 rm(list = ls())
 options(java.parameters="-Xmx50G")
 
-library(fastreeR)
+### load libraries
 
+library(fastreeR)
+library(ape)
 library(rJava)
+
+### increase java resources
 .jinit()
 num_gigs_ram_available = .jcall(.jnew("java/lang/Runtime"), "J", "maxMemory") / 1e9 
 paste("You have ", round(num_gigs_ram_available, 2), "GB memory available.", sep = "")
 
+### indicate vcf file
 vcfFile <- "path_to/short_TN_mito_snps-and-indels_clean_0.9_10x.vcf.gz"
- 
+
+### make tree from VCF
 vcftree<- vcf2tree(
     vcfFile,
     threads = 10,
@@ -136,3 +142,30 @@ vcftree<- vcf2tree(
   )
   
 vcftree
+
+### format output as tree file
+tree <- ape::read.tree(text = vcftree)
+
+## join the tree data with metadata from above
+x <- left_join(as_tibble(tree), tree_labels, by = c("label" = "name"))  %>% mutate(name=label)
+
+
+## make tree including clade labels.  Will have to change clade labels for data set.  Will need to find the nodes for each set of samples by uncommenting #geom_label(aes(label=node))
+
+
+p9 <- ggtree(tree) %<+% x  + 
+  geom_tippoint(aes(color = TumorP))  + coord_cartesian(clip = 'off') + 
+  geom_tiplab(aes(label=sample, color=TumorP, fontface="bold"),size=3, align=TRUE, linesize=.1) +
+  geom_nodelab( aes(label=label, subset=label > 79), hjust = 1.44, nudge_y = 0.65, size = 3) +
+  geom_nodelab( aes(label=label, subset=label ==100 ), hjust = 1.44, nudge_y = 0.65, size = 3) + coord_cartesian(clip = 'off') + 
+  theme_tree2(plot.margin=margin(10, 200, 10, 10))  + 
+  scale_color_manual(values=c("#3A9AB2","#F11B00","grey40")) + theme(legend.position="none") +
+  geom_cladelab(node=100, label="Lake Memphramegog HF Tumor Samples", align=TRUE, 
+                textcolor='black', barsize=2,barcolor="#3A9AB2", fontsize =2.5,offset=.03) + 
+  geom_cladelab(node=106, label="Reference Fish", align=TRUE, 
+                textcolor='black', barcolor="#000000", barsize=2,fontsize =2.5,offset=.03) + 
+  geom_cladelab(node=113, label="Lake Memphramegog Normal Samples", align=TRUE, barsize=2,
+                textcolor='black', barcolor="#F11B00",  fontsize =2.5,offset=.03) +
+  theme(axis.text.x = element_text(face="bold", size = 10)) 
+
+p9
